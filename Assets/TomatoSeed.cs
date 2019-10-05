ffb383214013b03589ca8efa11a24370f958097f
jakob <jakob.willforss@hotmail.com>
2019-10-05 11:22:04 +0200

    Planting in main

--- Assets/TomatoSeed.cs
+++ Assets/TomatoSeed.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class TomatoSeed : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
