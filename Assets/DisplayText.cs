31d09b8494c20a829062012faf8b155ba2af4220
Johan37 <johan.philipsson@hotmail.com>
2019-10-06 08:47:34 +0200

    Simple event handler

---
+++ Assets/DisplayText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+    public bool display_text;
+
+    private Panel panel;
+    private DisplayText displayText;
+    private Text text;
+
+    void Start () {
+        displayText = GetComponentInChildren<DisplayText>();
+        text = GetComponentInChildren<Text>();
+        text.text = "text";
+
+    }
+
+    public void ShowText(string message) {
+        text.text = message;
+    }
+}
