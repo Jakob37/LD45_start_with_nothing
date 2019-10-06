549b10b37d83fb2a1986ebd782eba1b5b2036323
danielhamngren <daniel.hamngren@gmail.com>
2019-10-06 17:59:26 +0200

    Tractor wheels are now rotating

---
+++ Assets/TractorWheel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TractorWheel : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float rotation_speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotation_speed);
+    }
+}
